cbb9c5926263f23a07ce0b55bb6f1cb01de3b38e
Diego Perez Gramond <diego.pegramm@gmail.com>
2024-01-26 05:49:44 +0000

    Added the report functionallity

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ print('-----------Menu--------------')
 for item in menu:
     print(item)
 
+
+def report():
+    print(f"Water: {resources['water']}")
+    print(f"Milk: {resources['milk']}")
+    print(f"Coffee: {resources['coffee']}")
+    
+
+earnings = 0
+resources = {
+    "water": 100,
+    "milk": 200,
+    "coffee": 80
+}
+
 is_on = True
 while is_on:
     user_input = input('What would you like? ')
+    
     if user_input == "off":
         is_on = False
+    
+    elif user_input == "report":
+        report()

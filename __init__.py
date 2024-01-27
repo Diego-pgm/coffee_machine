7a5270bcabc1d696e9e21c1c11dd5dbfa2bb1bba
Diego Perez Gramond <diego.pegramm@gmail.com>
2024-01-27 17:43:29 +0000

    changed the name of the main program

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from menu import menu
+from resources import resources, earnings
+from functions import check_resources, report, main
+
+
+print('-----------Menu--------------')
+for item in menu:
+    print(item)
+
+main()

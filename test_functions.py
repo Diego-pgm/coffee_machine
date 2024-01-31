d90b36e7eae69271b06124ddf9eea436875f08dd
Diego Perez Gramond <diego.pegramm@gmail.com>
2024-01-31 05:23:05 +0000

    fixed some errors

--- test_functions.py
+++ test_functions.py
@@ -7,18 +7,10 @@ test_drink = {
     "coffee": 100
 }
 
-
 def test_resources():
     assert check_resources(test_drink) == True
 
 
-def test_no_resources():
-    resources = {
-        "water": 0,
-        "milk": 0,
-        "coffee": 0
-    }
-    assert check_resources(test_drink) == False
-
 def test_report():
     assert report() == True
+

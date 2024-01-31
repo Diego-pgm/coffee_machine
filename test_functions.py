258b25af38c35df778e9fddb3c0a85bf86f32d45
Diego Perez Gramond <diego.pegramm@gmail.com>
2024-01-31 05:27:45 +0000

    code coverage for the case of not having resources

--- test_functions.py
+++ test_functions.py
@@ -7,10 +7,18 @@ test_drink = {
     "coffee": 100
 }
 
+def test_noresources():
+    global resources
+    resources['water'] = 0
+    resources['milk'] = 0
+    resources['coffee'] = 0
+    assert check_resources(test_drink) == False
+
 def test_resources():
     assert check_resources(test_drink) == True
 
 
 def test_report():
     assert report() == True
+
 
